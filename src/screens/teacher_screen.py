9d156d6c1cb12520f724ae56ee2c04b946005bc1
Yuvraj <yuvrajsajwan13@gmail.com>
2026-08-05 19:11:44 +0530

    updating ...

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -398,5 +398,3 @@ def teacher_screen_register():
     with btnc2:
         if st.button('Login Instead', type="primary", icon=':material/passkey:', width='stretch'):
             st.session_state.teacher_login_type = 'login'
-
-    # footer_dashboard()
