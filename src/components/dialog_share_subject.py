65e797999711c83dfc4cd95bc0454876d318e713
Yuvraj <yuvrajsajwan13@gmail.com>
2026-08-06 10:32:49 +0530

    Update app domain in share_subject_dialog function

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "ai-smart-attendance.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
