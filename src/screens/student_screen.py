744eb39c238234563fa8b70472e50387674714ae
Yuvraj <yuvrajsajwan13@gmail.com>
2026-08-06 08:18:40 +0530

    updating details.

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -146,7 +146,7 @@ def student_screen():
     if show_registration:
         with st.container(border=True):
             st.header('Register new Profile')
-            new_name = st.text_input("Enter your name", placeholder='E.g. Hamza Rizvi')
+            new_name = st.text_input("Enter your name", placeholder='E.g. Yuvraj')
 
             st.subheader('Optional : Voice Enrollment')
             st.info("Enroll your for voice only attendance")
